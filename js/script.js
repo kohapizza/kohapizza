2a0a8dfed6d8b985e2a305c41401d0d67b6cb88b
Koharu Saeki <g2120517@is.ocha.ac.jp>
2024-01-18 21:01:56 +0900

    要らないとこ消す

--- js/script.js
+++ js/script.js
@@ -1,22 +1,2 @@
 console.log("Hello 🌎");
 console.log("my name is koharu!");
-
-
-const btn = document.querySelector("button");
-if (btn) {
-  btn.onclick = function () {
-    btn.classList.toggle("dipped");
-  };
-}
-
-
-let goto = (file, line) => {
-  window.parent.postMessage(
-    { type: "glitch/go-to-line", payload: { filePath: file, line: line } }, "*"
-  );
-};
-
-const filer = document.querySelectorAll(".fileopener");
-filer.forEach((f) => {
-  f.onclick = () => { goto(f.dataset.file, f.dataset.line); };
-});
