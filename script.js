fae25b825e384f235edaf171430986fb7cd84bd7
Koharu Saeki <koharu0929alexandros@gmail.com>
2024-12-09 16:14:57 +0900

    一新するぞお

---
+++ script.js
@@ -0,0 +1,13 @@
+// スムーススクロールの実装
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+          window.scrollTo({
+              top: target.offsetTop - 50, // ヘッダーの高さを考慮
+              behavior: 'smooth',
+          });
+      }
+  });
+});
